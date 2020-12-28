42a753d82dac3ac0602f2ee7587724534ca6f69b
Joseph Cheng <indiejoseph@gmail.com>
2020-12-28 17:25:32 +0800

    update build scripts

---
+++ out/_next/static/Dy9W1JNDSE5twQvn4JhKo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F5423ad26.58392d2ceba23e8e1cf0.js","static\u002Fchunks\u002Fpages\u002Findex-212bf985845a53138ac0.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5bdfe767480352e01bfa.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
